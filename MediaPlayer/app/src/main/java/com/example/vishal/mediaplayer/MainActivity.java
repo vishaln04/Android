1a82c4d50ab118bee71af4835cafac3d7ad704aa
vishaln04 <vishalnagarale@gmail.com>
2016-12-12 18:21:11 +0530

    MediaPlayer Added

--- MediaPlayer/app/src/main/java/com/example/vishal/mediaplayer/MainActivity.java
+++ MediaPlayer/app/src/main/java/com/example/vishal/mediaplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.vishal.mediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlay = (Button) findViewById(R.id.play);
+        Button btnPause = (Button) findViewById(R.id.pause);
+        Button btnStop = (Button) findViewById(R.id.stop);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.idhedho_bagunde_cheli);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Play",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Pause",Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Stop",Toast.LENGTH_SHORT).show();
+                mediaPlayer.stop();
+               // MainActivity.this.finish();
+            }
+        });
     }
 }
